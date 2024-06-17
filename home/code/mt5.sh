1d34cffe355480126aa2a02a02edf283a7cf6abf
Gilbert Jeffrey <dsoyet@foxmail.com>
2024-06-17 15:06:45 +0800

    Update home/code/mt5.sh

--- home/code/mt5.sh
+++ home/code/mt5.sh
@@ -6,7 +6,16 @@
 URL="https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe"
 
 #mono for .net, geoko for IE
-pacman -Sy wine-mono wine-gecko wine-wow64
+#arch
+pacman -Sy wine-mono wine-gecko wine winetricks xorg-server-xvfb
+
+#debian
+sudo apt-get install xrdp icewm xfce4-terminal xvfb
+
+sudo apt-get install xrdp icewm xfce4-terminal xvfb build-essential
+
+sudo dpkg --add-architecture i386 && sudo apt update
+winetricks -q mt5.verb
 
 # Download MetaTrader
 wget $URL
